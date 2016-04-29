fe073fb08184234e00197b7eee6b319ae0ace28d
spxcds <jvbei@live.com>
2016-04-29 15:49:56 +0800

    设置环境
    -  数据库设置为MySQL
    -  新建customer App

--- ElecMall/settings.py
+++ ElecMall/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+ASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'customer.apps.CustomerConfig',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -76,8 +77,13 @@ WSGI_APPLICATION = 'ElecMall.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',
+        #'NAME': os.path.join(BASE_DIR, 'ElecMalldb'),
+        'NAME': 'ElecMalldb',
+        'USER': 'root',
+        'PASSWORD': 'ss175625',
+        'PORT': 3306,
     }
 }
 
--- makefile
+++ makefile
@@ -1,2 +1,4 @@
 all:
 	sudo python manage.py runserver 0.0.0.0:80
+check:
+	sudo python manage.py check
