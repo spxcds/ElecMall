5beec82ec9e7df10c79dcbd18c050ac06816ed38
spxcds <jvbei@live.com>
2016-05-06 15:05:37 +0800

    更改模板默认路径, 以及增加customer和ElecMall的模板

--- ElecMall/settings.py
+++ ElecMall/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'ElecMall.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join('templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- ElecMall/views.py
+++ ElecMall/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello, Welcome to the ElecMall!!")
+    context = {}
+    return render(request, 'ElecMall/index.html', context)
