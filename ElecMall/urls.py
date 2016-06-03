ea7bb13fbfe66763028c262bd13221565ef5c273
spxcds <jvbei@live.com>
2016-06-03 14:53:52 +0800

    增加首页逻辑

--- ElecMall/urls.py
+++ ElecMall/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 from . import views
+from django.views.generic import TemplateView
 from account.views import UserRegisterView, UserLoginView, UserLogoutView, UserEditView
 from utils.captcha.views import show_captcha
 
@@ -27,5 +28,19 @@ urlpatterns = [
     url(r'^account/login/$', UserLoginView.as_view()),
     url(r'^account/logout/$', UserLogoutView.as_view()),
     url(r'^account/settings/$', UserEditView.as_view()),
+    url(r'^goods/show/$', TemplateView.as_view(template_name='goods/demo.html')),
     url(r'^captcha/$', show_captcha),
+
+
+    url(r'^goods/index/$', TemplateView.as_view(template_name='goods/index.html')),
+    url(r'^goods/Apple/$', TemplateView.as_view(template_name='goods/Apple.html')),
+    url(r'^goods/Samsung/$', TemplateView.as_view(template_name='goods/Samsung.html')),
+    url(r'^goods/jianeng/$', TemplateView.as_view(template_name='goods/jianeng.html')),
+    url(r'^goods/kaxiou/$', TemplateView.as_view(template_name='goods/kaxiou.html')),
+    url(r'^goods/hp/$', TemplateView.as_view(template_name='goods/hp.html')),
+    url(r'^goods/imac/$', TemplateView.as_view(template_name='goods/imac.html')),
+    url(r'^goods/haixin/$', TemplateView.as_view(template_name='goods/haixin.html')),
+    url(r'^goods/tcl/$', TemplateView.as_view(template_name='goods/TCL.html')),
+    url(r'^goods/lianxiang/$', TemplateView.as_view(template_name='goods/lianxiangyitiji.html')),
+    url(r'^goods/hpyitiji/$', TemplateView.as_view(template_name='goods/hpyitiji.html')),
 ]
