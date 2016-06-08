30eafcc927fc28d15dd0d1db726ad25c16f83021
spxcds <jvbei@live.com>
2016-06-08 12:46:18 +0800

    修复浏览商品时无法显示用户昵称的视图错误

--- goods/views.py
+++ goods/views.py
@@ -6,10 +6,20 @@ from django.views.generic import View
 from utils.shortcuts import info_page
 
 from .models import Goods
+from account.decorators import login_required
+from account.models import Customer
 
 class GoodsShowView(View):
 
     def get(self, request, catagory):
+        try:
+            user_id = request.session.get("user_id", None)
+            request.user = Customer.objects.get(id=user_id)
+        except Customer.DoesNotExist:
+            pass
+
         goods = Goods.objects.filter(GoodsCatagory=catagory)
+        if not goods:
+            return info_page(request, "没有此类商品")
         context = {'goods': goods}
         return render(request, 'goods/index.html', context)
